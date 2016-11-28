df487b7fcdac07f2abdcd1e1998ef3b8adda6006
jgeofil <jeremy.geo@gmail.com>
2016-11-28 05:52:34 +0100

    100%

--- trees.py
+++ trees.py
@@ -2,15 +2,16 @@ import numpy as np
 import pandas
 from sklearn.ensemble import RandomForestClassifier
 from scipy.stats import wilcoxon
+from scipy.stats import mannwhitneyu
 from scipy.stats import chi2_contingency
 from sklearn.model_selection import LeaveOneOut
 
-SNPS = 5000
+SNPS = 800000
 R = 30
-mtry = 0.4
+mtry = 0.5
 THETA = 0.05
-THETA_CHI = 0.000001
-RE_RANK = True
+THETA_CHI = 0.00000005
+RE_RANK = False
 
 LABELS = np.array([0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,3,3,
 3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,7,7,7,7,7,7,7,
@@ -39,7 +40,7 @@ if RE_RANK:
     print 'Generating '+str(R)+' replicate forests...'
     RImportanceScores = []
     for x in range(R):
-        clf = RandomForestClassifier(max_features=int(mtry*SNPS*2), n_jobs=-1, verbose=True)
+        clf = RandomForestClassifier(n_estimators=100, max_features=int(mtry*SNPS*2), n_jobs=-1, verbose=True)
         clf.fit(data, LABELS)
         RImportanceScores.append(clf.feature_importances_)
     RImportanceScores = np.array(RImportanceScores)
@@ -52,16 +53,21 @@ if RE_RANK:
 
     print RImportanceScores
 
-    for SNPScores in RImportanceScores[:,:SNPS].T:
-        _,p = wilcoxon(shadowImportanceMax,SNPScores)
-        #TODO: Wilcoxon test is two sided...........!
-        pValues.append(p)
+    for SNPScores, shadow in zip(RImportanceScores[:,:SNPS].T, RImportanceScores[:,SNPS:].T):
+        if not np.array_equal(SNPScores, shadow):
+            #_,p = mannwhitneyu(SNPScores, shadow, alternative='greater') #1425
+            _,p = wilcoxon(SNPScores, shadow) #603
+            #_,p = wilcoxon(SNPScores, shadowImportanceMax)
+            #TODO: Wilcoxon test is two sided...........!
+            pValues.append(p)
+        else:
+            pValues.append(float('inf'))
 
     print 'Ranking SNPs...'
     pValueIndexes = []
     print pValues
     for x,val in enumerate(pValues):
-        if val >= THETA: #TODO: Weird
+        if val <= THETA: #TODO: Weird
             pValueIndexes.append(x)
 
     WilPassedSNPs = data[:,pValueIndexes]
@@ -79,11 +85,11 @@ SNPChiSquared = []
 for SNP in WilPassedSNPs.T:
     table = np.zeros(shape=(2, N_LABEL), dtype=int)
     for s,l in zip(SNP, LABELS):
-        table[s,l] += 1
+        table[int(s),int(l)] += 1
     _,p,_,_ = chi2_contingency(table) #TODO: check parameters of this..
     SNPChiSquared.append(p)
 
-'''
+
 print 'Ranking SNPs...'
 pValueIndexes = []
 notpValueIndexes = []
@@ -97,13 +103,13 @@ GreatSNPs = WilPassedSNPs[:,pValueIndexes]
 OkSNPs = WilPassedSNPs[:,notpValueIndexes]
 print OkSNPs.shape
 print GreatSNPs.shape
-'''
+
 
 loo = LeaveOneOut()
 res = []
 proba = []
-for train, test in loo.split(WilPassedSNPs):
-    clf = RandomForestClassifier(n_estimators=10, verbose=True, n_jobs=-1)
+for train, test in loo.split(np.concatenate((OkSNPs,GreatSNPs,GreatSNPs), axis=1)):
+    clf = RandomForestClassifier(n_estimators=100, verbose=True, n_jobs=-1)
     clf.fit(WilPassedSNPs[train], LABELS[train])
     res.append(clf.predict(WilPassedSNPs[test])[0])
     proba.append(clf.predict_proba(WilPassedSNPs[test])[0])
